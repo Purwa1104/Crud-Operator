b2075162ace4270bfb29447403bdff3d601e154a
Purwa Prakash <prakashpurwa@gmail.com>
2024-06-25 23:04:10 +0530

    crud_operator

---
+++ src/EmployeeData.js
@@ -0,0 +1,20 @@
+export const employeeData = [
+    {
+        id: 1,
+        firstName: "Rahul",
+        lastName: "Dravid",
+        age: 45
+    },
+    {
+        id: 2,
+        firstName: "Virat",
+        lastName: "Kohli",
+        age: 35
+    },
+    {
+        id: 3,
+        firstName: "Rohit",
+        lastName: "Sharma",
+        age: 36
+    }
+]
